81755c9d442cf7dec2fa91c92e0f214b30730456
FannBott <108573537+FannBott@users.noreply.github.com>
2022-10-29 20:10:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['66827294199','6281350392168','6283875342432']
+global.owner = ['66827294199','6283892675760','6285930193166']
 global.premium = ['66827294199']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
