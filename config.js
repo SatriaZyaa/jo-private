cce45e68d30582630fea4700651f4d22d245be98
SatriaZyaa <116971290+SatriaZyaa@users.noreply.github.com>
2022-10-31 10:49:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['66827294199','6283875342432','79041971687']
+global.owner = ['66827294199','6283875342432','6288215752313']
 global.premium = ['66827294199']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
